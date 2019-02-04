1a306f773703275b4cf0c8ffaeedd944360182f9
Marifel Angeles <mmarifel@gmail.com>
2019-02-04 11:55:12 -0600

    add new player to DOM

--- client.js
+++ client.js
@@ -1,5 +1,27 @@
 $(document).ready(docReady);
 
-function docReady() {
+function addRockPicker(){
+    console.log('in addRockPicker');
     
+    // on add new picker, get name
+    let name = $('#nameInput').val();
+    console.log('input:', name);
+
+    // append on rockPickerTable
+    $('#rockPickerTable').append(`
+            <tr class="playerRow">
+                <td class="name">${name}</td>
+                <td class="score"><span class="numberOfRocks">0</span></td>
+                <td><button class="addButton">Add 1</button></td>
+                <td><button class="minusButton">Minus 1</button></td>
+                <td><button class="resetButton">Reset to 0</button></td>
+                <td><button class="deleteButton">Delete</button></td>
+            </tr>
+    `);
+} // end addRockPicker
+
+function docReady() {
+    // add new picker
+    $('#addRockPickerButton').on('click', addRockPicker);
+        
 } // end docReady
